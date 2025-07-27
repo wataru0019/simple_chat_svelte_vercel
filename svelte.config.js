c00e704e730848552b0084c29ca90bf006539da5
WataruKashihara <kashiwabarawataru@kashiwabarawatarunoMacBook-Pro.local>
2025-07-27 14:04:31 +0900

    vercel deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,32 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	// Consult https://svelte.dev/docs/kit/integrations
+// 	// for more information about preprocessors
+// 	preprocess: vitePreprocess(),
+
+// 	kit: {
+// 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+// 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+// 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+// 		adapter: adapter()
+// 	}
+// };
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// オプション設定
+			runtime: 'nodejs18.x', // またはnodejs20.x
+			regions: ['nrt1'], // 東京リージョン（日本の場合）
+			split: false
+		})
 	}
 };
+
 
 export default config;
